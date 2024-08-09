f584c537a99f7a5b92b64009addea026480a5f8d
cschasestevens <chase@havensight.net>
2024-08-09 14:33:44 -0400

    Initial commit

--- R/1.2.params.R
+++ R/1.2.params.R
@@ -5,75 +5,106 @@
 #' @param f Path to the name of a data file in the "analysis/" folder. Accepts .xlsx, .csv, and .txt files.
 #' @param md.num Number of metadata columns in input data file.
 #' @param qc.rep Should a QC report be generated for the selected dataset? (TRUE/FALSE)
+#' @param norm1 Logical indicating if the data have been normalized.
 #' @return List containing formatted input data and metadata for downstream analysis.
 #' @examples
 #'
 #' # # Dataset Input
-#' # ms.input("example.xlsx",4,FALSE)
+#' # ms.input("example.xlsx",4,FALSE,TRUE)
 #'
 #' @export
 ms.input <- function(
     f,
-    md.num
+    md.num,
+    qc.rep,
+    norm1
     ) {
   if(tools::file_ext(f) == "xlsx"){
-    d <- readxl::read_excel(f,sheet = 1)
+    d <- readxl::read_excel(paste("data/","example.xlsx",sep = ""),sheet = 1)
+    a1 <- data.frame(
+      "ID" = seq.int(
+        1,
+        length(names(d[,(md.num+1):ncol(d)])),
+        1),
+      "name" = names(d[,(md.num+1):ncol(d)]),
+      "type" = ifelse(
+        grepl(
+          "iSTD|1_CE 22:1|1_Sphingosine d17:1",
+          names(d[,(md.num+1):ncol(d)])),
+        "iSTD",
+        "an.comp"
+        )
+      )
     list.params <- list(
-      "data" = d[,(md.num+1):ncol(d)],
+      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
       "meta" = d[,1:md.num],
-      "qc" = qc.rep
+      "anno" = a1,
+      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
+      "qc" = qc.rep,
+      "nrm" = norm1
       )
     }
   if(tools::file_ext(f) == "csv"){
     d <- read_csv(
-      f,
+      paste("data/",f,sep = ""),
       check.names = T,
       header = T
       )
+    a1 <- data.frame(
+      "ID" = seq.int(
+        1,
+        length(names(d[,(md.num+1):ncol(d)])),
+        1),
+      "name" = names(d[,(md.num+1):ncol(d)]),
+      "type" = ifelse(
+        grepl(
+          "iSTD|1_CE.22.1|1_Sphingosine.d17.1",
+          names(d[,(md.num+1):ncol(d)])),
+        "iSTD",
+        "an.comp"
+      )
+    )
     list.params <- list(
-      "data" = d[,(md.num+1):ncol(d)],
+      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
       "meta" = d[,1:md.num],
-      "qc" = qc.rep
+      "anno" = a1,
+      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
+      "qc" = qc.rep,
+      "nrm" = norm1
     )
   }
   if(tools::file_ext(f) == "txt"){
     d <- read_table(
-      f,
+      paste("data/",f,sep = ""),
       check.names = T,
       header = T,
       sep = "\t"
       )
+    a1 <- data.frame(
+      "ID" = seq.int(
+        1,
+        length(names(d[,(md.num+1):ncol(d)])),
+        1),
+      "name" = names(d[,(md.num+1):ncol(d)]),
+      "type" = ifelse(
+        grepl(
+          "iSTD|1_CE.22.1|1_Sphingosine.d17.1",
+          names(d[,(md.num+1):ncol(d)])),
+        "iSTD",
+        "an.comp"
+      )
+    )
     list.params <- list(
-      "data" = d[,(md.num+1):ncol(d)],
+      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
       "meta" = d[,1:md.num],
-      "qc" = qc.rep
+      "anno" = a1,
+      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
+      "qc" = qc.rep,
+      "nrm" = norm1
     )
   }
-
-
-
-  list.params <- data.frame(
-    # Universal columns
-    data.frame(
-      # Sample Number
-      Sample.No = seq(1:length(
-        basename(
-          list.files(d.path)
-          )
-        )
-      ),
-      # Individual file names (uses CellRanger folder name by default)
-      File.ID = basename(list.files(d.path)),
-      # Data file paths (Location of CellRanger files: 'Data/' by default)
-      Path = paste(d.path,list.files(d.path),sep = ""),
-      # Path to feature files
-      Path.feat = paste(d.path,list.files(d.path),"/filtered_feature_bc_matrix/features.tsv.gz",sep = "")
-      ),
-
-    # Dataset-specific columns
-    study.md
-    )
-  }
+  return(list.params)
+}
 
 
 
