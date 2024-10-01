9f5be663c5e4dc6eeef71944d74a8db8d8126469
cschasestevens <chase@havensight.net>
2024-10-01 11:35:37 -0400

    Initial commit

--- R/1.2.params.R
+++ R/1.2.params.R
@@ -1,113 +1,142 @@
 #' Data Input
 #'
-#' Creates a data frame of processing parameters used in scRNA-Seq data processing by Seurat
+#' Creates a data frame of processing parameters
+#' used for data processing and analysis.
 #'
-#' @param f Path to the name of a data file in the "analysis/" folder. Accepts .xlsx, .csv, and .txt files.
-#' @param md.num Number of metadata columns in input data file.
-#' @param qc.rep Should a QC report be generated for the selected dataset? (TRUE/FALSE)
+#' @param f Name of a data file in the "data/" folder.
+#' Accepts .xlsx, .csv, and .txt files.
+#' @param md_num Number of metadata columns in input data file.
+#' @param qc_rep Should a QC report be generated for the selected dataset?
+#' (Either TRUE or FALSE)
 #' @param norm1 Logical indicating if the data have been normalized.
-#' @param ref1 Reference annotation list for assigning compound details/class information (provided as a .txt file).
-#' @return List containing formatted input data and metadata for downstream analysis.
+#' (Either TRUE or FALSE)
+#' @param ref1 Reference annotation list for assigning compound details and
+#' class information (provided as a .txt file).
+#' @return List containing formatted input data and metadata
+#' for downstream analysis.
 #' @examples
 #'
-#' # # Dataset Input
-#' # ms.input("example.xlsx",4,FALSE,TRUE)
+#' # ms_input(
+#' #   "example.xlsx",
+#' #   4,
+#' #   FALSE,
+#' #   TRUE
+#' # )
 #'
 #' @export
-ms.input <- function(
-    f,
-    md.num,
-    qc.rep,
-    norm1,
-    ref1
-    ) {
-  if(tools::file_ext(f) == "xlsx"){
-    d <- readxl::read_excel(paste("data/","example.xlsx",sep = ""),sheet = 1)
+ms_input <- function(
+  f,
+  md_num,
+  qc_rep,
+  norm1,
+  ref1
+) {
+  if(tools::file_ext(f) == "xlsx") { # nolint
+    d <- readxl::read_excel(
+      paste(
+        "data/",
+        f,
+        sep = ""
+      ),
+      sheet = 1
+    )
     a1 <- data.frame(
       "ID" = seq.int(
         1,
-        length(names(d[,(md.num+1):ncol(d)])),
-        1),
-      "name" = names(d[,(md.num+1):ncol(d)]),
+        length(names(d[, (md_num + 1):ncol(d)])),
+        1
+      ),
+      "name" = names(d[, (md_num + 1):ncol(d)]),
       "type" = ifelse(
         grepl(
           "iSTD|1_CE 22:1|1_Sphingosine d17:1",
-          names(d[,(md.num+1):ncol(d)])),
-        "iSTD",
-        "an.comp"
-        )
-      )
-    list.params <- list(
-      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
-      "meta" = d[,1:md.num],
-      "anno" = a1,
-      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
-      "qc" = qc.rep,
-      "nrm" = norm1
-      )
-    }
-  if(tools::file_ext(f) == "csv"){
-    d <- read_csv(
-      paste("data/",f,sep = ""),
-      check.names = T,
-      header = T
-      )
-    a1 <- data.frame(
-      "ID" = seq.int(
-        1,
-        length(names(d[,(md.num+1):ncol(d)])),
-        1),
-      "name" = names(d[,(md.num+1):ncol(d)]),
-      "type" = ifelse(
-        grepl(
-          "iSTD|1_CE.22.1|1_Sphingosine.d17.1",
-          names(d[,(md.num+1):ncol(d)])),
+          names(d[, (md_num + 1):ncol(d)])
+        ),
         "iSTD",
         "an.comp"
       )
     )
-    list.params <- list(
-      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
-      "meta" = d[,1:md.num],
+    lpar <- list(
+      "data" = d[, -c(1:md_num, a1[a1[["type"]] == "iSTD", "ID"] + md_num)],
+      "meta" = d[, 1:md_num],
       "anno" = a1,
-      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
-      "qc" = qc.rep,
+      "istd" = d[, a1[a1[["type"]] == "iSTD", "ID"] + md_num],
+      "qc" = qc_rep,
       "nrm" = norm1
     )
   }
-  if(tools::file_ext(f) == "txt"){
-    d <- read_table(
-      paste("data/",f,sep = ""),
-      check.names = T,
-      header = T,
+  if(tools::file_ext(f) == "csv") { # nolint
+    d <- read.csv(
+      paste(
+        "data/",
+        f,
+        sep = ""
+      ),
+      check.names = TRUE,
+      header = TRUE
+    )
+    a1 <- data.frame(
+      "ID" = seq.int(
+        1,
+        length(names(d[, (md_num + 1):ncol(d)])),
+        1
+      ),
+      "name" = names(d[, (md_num + 1):ncol(d)]),
+      "type" = ifelse(
+        grepl(
+          "iSTD|1_CE.22.1|1_Sphingosine.d17.1",
+          names(d[, (md_num + 1):ncol(d)])
+        ),
+        "iSTD",
+        "an.comp"
+      )
+    )
+    lpar <- list(
+      "data" = d[, -c(
+        1:md_num,
+        a1[a1[["type"]] == "iSTD", "ID"] + md_num
+      )],
+      "meta" = d[, 1:md_num],
+      "anno" = a1,
+      "istd" = d[, a1[a1[["type"]] == "iSTD", "ID"] + md_num],
+      "qc" = qc_rep,
+      "nrm" = norm1
+    )
+  }
+  if(tools::file_ext(f) == "txt") { # nolint
+    d <- read.table(
+      paste(
+        "data/",
+        f,
+        sep = ""
+      ),
+      check.names = TRUE,
+      header = TRUE,
       sep = "\t"
-      )
+    )
     a1 <- data.frame(
       "ID" = seq.int(
         1,
-        length(names(d[,(md.num+1):ncol(d)])),
-        1),
-      "name" = names(d[,(md.num+1):ncol(d)]),
+        length(names(d[, (md_num + 1):ncol(d)])),
+        1
+      ),
+      "name" = names(d[, (md_num + 1):ncol(d)]),
       "type" = ifelse(
         grepl(
           "iSTD|1_CE.22.1|1_Sphingosine.d17.1",
-          names(d[,(md.num+1):ncol(d)])),
+          names(d[, (md_num + 1):ncol(d)])),
         "iSTD",
         "an.comp"
       )
     )
-    list.params <- list(
-      "data" = d[,-c(1:md.num,a1[a1[["type"]] == "iSTD","ID"] + md.num)],
-      "meta" = d[,1:md.num],
+    lpar <- list(
+      "data" = d[, -c(1:md_num, a1[a1[["type"]] == "iSTD", "ID"] + md_num)],
+      "meta" = d[, 1:md_num],
       "anno" = a1,
-      "istd" = d[,a1[a1[["type"]] == "iSTD","ID"] + md.num],
-      "qc" = qc.rep,
+      "istd" = d[, a1[a1[["type"]] == "iSTD", "ID"] + md_num],
+      "qc" = qc_rep,
       "nrm" = norm1
     )
   }
-  return(list.params)
+  return(lpar)
 }
-
-
-
-
